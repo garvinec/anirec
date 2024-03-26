996a12459a527b2644f14b1551ccbafd680ddd35
garvinec <chan.gar@northeastern.edu>
2024-03-26 19:20:58 -0400

    Adds all the pages and wired up the fetch data from api logic

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,23 +3,75 @@ const router = express.Router();
 const topAnimeData = require("../utils/animeData");
 
 router.get("/", (req, res) => {
-  res.render("index", {
+  return res.render("pages/home", {
     title: "Homepage Title",
     content: "Some random content",
   });
 });
 
-router.get("/anime", (req, res) => {
-  const pageNumber = req.query.page ?? "1";
-  topAnimeData(pageNumber, (response, error) => {
-    if (error) {
-      return res.send(error);
-    }
-    const pagination = response.pagination;
-    const data = response.data;
-
-    res.send(response);
+router.get("/action", (req, res) => {
+  return res.render("pages/action", {
+    title: "Action Page Title",
+    content: "Some random content",
   });
+});
+
+router.get("/adventure", (req, res) => {
+  return res.render("pages/adventure", {
+    title: "Adventure Page Title",
+    content: "Some random content",
+  });
+});
+
+router.get("/comedy", (req, res) => {
+  return res.render("pages/comedy", {
+    title: "Comedy Page Title",
+    content: "Some random content",
+  });
+});
+
+router.get("/romance", (req, res) => {
+  // res.render("pages/romance", {
+  //   title: "Romance Page Title",
+  //   content: "Some random content",
+  // });
+  topAnimeData(
+    "Romance",
+    (response) => {
+      res.send(response);
+    },
+    (error) => {
+      res.send(error);
+    }
+  );
+});
+
+router.get("/sci-fi", (req, res) => {
+  // res.render("pages/scifi", {
+  //   title: "Sci-fi Page Title",
+  //   content: "Some random content",
+  // });
+  topAnimeData(
+    "Sci-Fi",
+    (response) => {
+      res.send(response);
+    },
+    (error) => {
+      res.send(error);
+    }
+  );
+});
+
+router.get("/anime", (req, res) => {
+  topAnimeData(
+    "",
+    (response) => {
+      res.send(response);
+    },
+    (error) => {
+      res.send(error);
+    }
+  );
 });
 
 module.exports = router;
--- src/utils/animeData.js
+++ src/utils/animeData.js
@@ -1,18 +1,65 @@
 const axios = require("axios");
-const { response } = require("express");
 
-const topAnimeData = (page, successCallback, failureCallback) => {
-  const requestURL = `https://api.jikan.moe/v4/top/anime?page=${page}`;
-  console.log("GET -", requestURL);
+const topAnimeData = async (genre, successCallback, failureCallback) => {
+  const animeList = [];
+  var page = 1;
+  while (animeList.length <= 50) {
+    let pagination = null;
+    let data = null;
+    let hasError = false;
+    let error = null;
+    var requestURL = `https://api.jikan.moe/v4/top/anime?page=${page.toString()}`;
 
-  axios
-    .get(requestURL)
-    .then((res) => {
-      const data = res.data;
-      successCallback(data);
-      return;
-    })
-    .catch((err) => failureCallback(err.data));
+    await axios
+      .get(requestURL)
+      .then((res) => {
+        pagination = res.data.pagination;
+        data = res.data.data;
+        for (let i = 0; (len = data.length), i < len; i++) {
+          if (genre) {
+            var genres = data[i].genres;
+            var animeGenres = [];
+            for (let j = 0; (len = genres.length), j < len; j++) {
+              animeGenres.push(genres[j].name);
+            }
+            if (animeGenres.includes(genre)) {
+              animeList.push(data[i].title);
+            }
+          } else {
+            animeList.push(data[i].title);
+          }
+        }
+        console.log("GET -", requestURL);
+      })
+      .catch((err) => {
+        hasError = true;
+        error = err;
+        console.log("GET FAIL -", requestURL);
+        console.log(
+          `Error ${error.response.status}: ` + error.response.statusText
+        );
+      });
+
+    if (hasError) {
+      if (error.response.status !== 429) {
+        // If it's not too many requests error, failureCallback
+        failureCallback(
+          `Error ${error.response.status}: ` + error.response.statusText
+        );
+        return;
+      }
+      break;
+    }
+
+    // Check if there's more data. Otherwise, out of data to fetch, leave loop
+    if (pagination.has_next_page) {
+      page++;
+    } else {
+      break;
+    }
+  }
+
+  successCallback(animeList);
 };
 
 module.exports = topAnimeData;
